e7635f9244092fbbcb7bfae6127774a00de123e4
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-04-18 22:10:58 +0300

    update

---
+++ PortfolioCore/ViewComponents/_DefaultTestimonialsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.ViewComponents
+{
+    public class _DefaultTestimonialsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
